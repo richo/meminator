07f0cad53322934202afb97b5eb383285dbd47b3
richo <richo@psych0tik.net>
2012-05-08 20:53:16 +1000

    Bump version (again)
    (Kill me)

--- lib/meminator.rb
+++ lib/meminator.rb
@@ -6,7 +6,7 @@ require 'json'
 # Generate memes using http://memegenerator.net
 
 module Meminator
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
   GENERATOR_URL = 'http://version1.api.memegenerator.net/Instance_Create'
   DISPLAY_URL = 'http://cdn.memegenerator.net'
   class Error < Exception; end
